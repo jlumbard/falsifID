930dcaf732ee253577e14921136270b12af332ab
jlumbard <jlumbard@uwo.ca>
2019-02-26 09:22:17 -0500

    Added Image Upload Component to Screen

--- app/index.js
+++ app/index.js
@@ -1,7 +1,60 @@
-import React from 'react';
-import WelcomeIcon from './components/WelcomeIcon';
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+import ImageSelect from './components/ImageSelect';
+import Expo from 'expo';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+
+    Expo.Permissions.askAsync(Expo.Permissions.CAMERA_ROLL);
 
 
-export default () => (
-    <WelcomeIcon></WelcomeIcon>
-)
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#4F6D7A"
+        />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <ImageSelect/>
+        <ImageSelect/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4F6D7A',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#F5FCFF',
+    marginBottom: 5,
+  },
+});
