464543e0fe17e09d173fe951dd69172f3287ec7f
jlumbard <jlumbard@uwo.ca>
2019-02-27 16:37:07 -0500

    hacked together the image add

--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -6,11 +5,19 @@ import {
   Text,
   View,
   StatusBar,
-  Button
+  Button,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 import HeaderIcon from './components/HeaderIcon';
-import ImageSelect from './components/ImageSelect';
+import { ImagePicker } from 'expo';
 import Expo from 'expo';
+
+const options={
+  title: 'my pic app',
+  takePhotoButtonTitle: 'Take photo with your camera',
+  chooseFromLibraryButtonTitle: 'Choose photo from library',
+}
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -20,17 +27,64 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+
+  state = {
+    image: null,
+    imageData:null,
+    IDimage: null,
+    IDimageData:null,
+  };
+
+  imageAdd = async (caller) =>{
+    let result = await ImagePicker.launchImageLibraryAsync({
+      //Below are preferences for the image picking
+      allowsEditing: true,
+      aspect: [3, 3],
+      mediaTypes: Expo.ImagePicker.MediaTypeOptions.Images,
+      base64: true,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      console.log(caller);
+      if(caller =="ID"){
+        this.setState({ IDimage: result.uri, IDimageData: result.base64 });
+      }
+      else if( caller == "notID"){
+      this.setState({ image: result.uri, imageData: result.base64 });
+      }
+    }
+  }
+
+
   render() {
 
     Expo.Permissions.askAsync(Expo.Permissions.CAMERA_ROLL);
-
-
+    
+    
     return (
+
       <View style={styles.container}>
         <StatusBar barStyle="dark-content"/>
         <HeaderIcon style={styles.logo}/>
-        <ImageSelect style={styles.image}/>
-        <ImageSelect style={styles.image}/>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#2e2e2e', borderRadius: 30 }}>
+        <Button
+          title="Add Photo"
+          onPress={() => this.imageAdd("notID")}
+        />
+        {this.state.image &&
+          <Image source={{ uri: this.state.image }} style={{ width: 200, height: 200 }} />}
+        </View> 
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#2e2e2e', borderRadius: 30 }}>
+        <Button
+          title="Add ID Photo"
+          onPress={() => this.imageAdd("ID")}
+        />
+        {this.state.IDimage &&
+          <Image source={{ uri: this.state.IDimage }} style={{ width: 200, height: 200 }} />}
+      </View>
         <View style="display:inline">
             <Button title="Calculate"></Button>
             <Text>0% Match</Text>
