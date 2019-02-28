97a14594d2ad3a8e9d7bf39ac7d0124593df3ca3
jlumbard <jlumbard@uwo.ca>
2019-02-27 22:52:28 -0500

    added the non-working code for tfjs import

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
 import App from './app/index'
 
+/* import '@tensorflow/tfjs-node';
+
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+import * as canvas from 'canvas';
+
+import * as faceapi from 'face-api.js';
+
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement, additionally an implementation
+// of ImageData is required, in case you want to use the MTCNN
+const { Canvas, Image, ImageData } = canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
+ */
+
+ //The above currently non-working, need to figure out if theres a workaround for using FS
+
 export default App;
